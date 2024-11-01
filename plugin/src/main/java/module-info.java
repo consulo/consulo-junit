801373977485e650ae41dd1b12bb754a092ba959
UNV <nikolay@yurchenko.su>
2024-11-01 21:03:18 +0300

    Reformatting pom.xml and module-info.java with IJ4 code style.

--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -4,25 +4,28 @@
  */
 module com.intellij.junit
 {
-  requires consulo.ide.api;
+    requires consulo.ide.api;
 
-  requires consulo.java.indexing.api;
-  requires consulo.java.analysis.impl;
-  requires consulo.java.analysis.api;
-  requires consulo.java.execution.api;
-  requires consulo.java.execution.impl;
-  requires consulo.java;
+    requires consulo.java.indexing.api;
+    requires consulo.java.analysis.impl;
+    requires consulo.java.analysis.api;
+    requires consulo.java.execution.api;
+    requires consulo.java.execution.impl;
+    requires consulo.java;
 
-  requires com.intellij.xml;
+    requires com.intellij.xml;
 
-  requires com.intellij.junit5.rt;
-  requires com.intellij.junit.rt;
+    requires com.intellij.junit5.rt;
+    requires com.intellij.junit.rt;
 
-  // TODO remove in future
-  requires java.desktop;
-  requires forms.rt;
+    // TODO remove in future
+    requires java.desktop;
+    requires forms.rt;
 
-  opens com.intellij.execution.junit to consulo.component.impl, consulo.util.xml.serializer;
+    opens com.intellij.execution.junit to
+        consulo.component.impl,
+        consulo.util.xml.serializer;
 
-  opens consulo.junit.inspection to consulo.util.xml.serializer;
+    opens consulo.junit.inspection to
+        consulo.util.xml.serializer;
 }
